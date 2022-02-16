6238ea80bf4beb959c81879ffe5b5aac4cca23ed
IT21298912 <99547060+IT21298912@users.noreply.github.com>
2022-02-16 08:59:36 +0000

    i wrote a formula to get the average of two numbers and displayed it

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+  int m1,m2;
+  float avg;
+
+  printf("enter marks::");
+  scanf("%d",&m1);
+
+  printf("enter marks::");
+  scanf("%d",&m2);
+
+  avg= (m1+m2)/2;
+
+  printf("%f",avg);
+  
   
   return 0;
 }
