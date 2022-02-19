b96d74240a2fcf3a84e642c31183be750e534ece
IT21298912 <99547060+IT21298912@users.noreply.github.com>
2022-02-19 03:56:43 +0000

    i used while loop

--- Tute03.c
+++ Tute03.c
@@ -13,6 +13,18 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+  int n,sum=0;
+
+  printf("enter a number:");
+  scanf("%d",&n);
+
+  while(n>0)
+    {
+      sum=sum+n;
+      n--;
+    }
+
+  printf("sum is %d",sum);
   
   return 0;
 }
